3db17346e00dfec9599f8da499d0d94293ade1f9
apoorwaG <apoogiri@gmail.com>
2022-03-29 19:05:43 -0700

    implemented dummy add project and add to do item functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './css/style.css';
 
 // toDo item factory
-const toDoFactory = (title, description, dueDate, priority, status=false) => {
+const toDoFactory = (id = null, title, description, dueDate, priority, status = false) => {
 
+    const getId = () => id;
     const getTitle = () => title;
     const getDescription = () => description;
     const getDueDate = () => dueDate;
@@ -10,61 +11,86 @@ const toDoFactory = (title, description, dueDate, priority, status=false) => {
     const getStatus = () => status;
 
     const summarize = () => {
-        const summary = `Title: ${title}\nDescription: ${description}\nDue Date: ${dueDate}\nPriority: ${priority}`;
+        const summary = `Id: ${id}\nTitle: ${title}\nDescription: ${description}\nDue Date: ${dueDate}\nPriority: ${priority}`;
         return summary;
     }
 
-    return {getTitle, getDescription, getDueDate, getPriority, getStatus, summarize};
+    return { getId, getTitle, getDescription, getDueDate, getPriority, getStatus, summarize };
 };
 
 // project factory
 const projectFactory = (name) => {
+    const toDos = [];
+    let numToDos = 0;
+
     const getName = () => name;
 
-    const toDos = [];
-
-    const addToProject = (toDo) => {
+    const add = (toDo) => {
         toDos.push(toDo);
+        numToDos++;
     };
 
-    const removeFromProject = (itemIndex) => {
-        if(itemIndex > toDos.length){
-            throw new Error('Index out of bounds!');
+    const remove = (itemIndex) => {
+        if (!Boolean(toDos[itemIndex])) {
+            throw new Error("Cannot find item in this project!");
         }
         delete toDos[itemIndex];
+        numToDos--;
     };
 
-    const printItems = () => {
+    const visualize = () => {
         toDos.forEach((item) => {
             console.log(item.summarize());
         })
     };
 
-    return {getName, addToProject, removeFromProject, printItems};
+    const getToDos = () => toDos;
+
+    const getNumToDos = () => numToDos;
+
+    return { getName, add, remove, getToDos, getNumToDos, visualize };
 };
 
 
 // internal projects module
 const toDoModule = (() => {
     const projects = [];
+    let numProjects = 0;
+
+    const getNumProjects = () => numProjects;
 
     // create a new project and add to projects list
     const addProject = (projectName) => {
         projects.push(projectFactory(projectName));
+        numProjects += 1;
     };
 
     // remove an existing project given its id
     const removeProject = (projectId) => {
-        if(projectId > projects.length) {
-            throw new Error("Index out of bounds!");
+        if (!Boolean(projects[projectId])) {
+            throw new Error("Cannot remove an a project that doesn't exist");
         }
         // projectId is the index of the project in the array as well as the projectId data attribute of the DOM node 
         delete projects[projectId];
+        numProjects--;
     };
 
-    // add a toDo item to an existing project
+    // display all items on a project and return the project
+    const viewProject = (projectId) => {
+        if (!Boolean(projects[projectId])) {
+            throw new Error("Cannot view a project that doesn't exist");
+        }
+        console.log(`Project name: ${projects[projectId].getName()}`);
+        projects[projectId].visualize();
+        return projects[projectId];
+    };
+
+    // get number of items/toDos in a project
+    const getNumToDos = (projectId) => projects[projectId].getNumToDos();
+
+    // add a toDo item to an existing project and return the id
     const addToProject = (projectId, item) => {
-        if(!Boolean(projects[projectId])) {
+        if (!Boolean(projects[projectId])) {
             throw new Error("Cannot add an item to a project that doesn't exist");
         }
         // item has title, description, dueDate, and priority attributes
@@ -80,43 +106,150 @@ const toDoModule = (() => {
         } catch (error) {
             throw error;
         }
-        const toDo = toDoFactory(title, description, dueDate, priority);
-        projects[projectId].addToProject(toDo);
+        const id = getNumToDos(projectId);
+        const toDo = toDoFactory(id, title, description, dueDate, priority);
+        projects[projectId].add(toDo);
+        return id;
     };
 
     // remove a toDo item from an existing project
     const removeFromProject = (projectId, itemId) => {
-        if(!Boolean(projects[projectId])) {
-            throw new Error("Cannot add an item to a project that doesn't exist");
+        if (!Boolean(projects[projectId])) {
+            throw new Error("Cannot remove an item from a project that doesn't exist");
         }
-        projects[projectId].removeFromProject(itemId);
+        projects[projectId].remove(itemId);
 
     };
 
+    return { getNumProjects, addProject, removeProject, viewProject, getNumToDos, addToProject, removeFromProject };
 
-    return {addProject, removeProject, addToProject, removeFromProject};
+})();
+
+// DOM controller; adds stuff to and removes stuff from dom
+// controls the event listeners as well
+const displayController = (() => {
+    const projectsSection = document.querySelector('.projects');
+
+    // add new project to display and return the node
+    const addProject = (projectName, projectId) => {
+        const project = document.createElement("div");
+        project.textContent = projectName;
+        project.setAttribute("data-projectid", `${projectId}`);
+        projectsSection.appendChild(project);
+        return project;
+    };
+
+    const deleteAllItems = (projectNode) => {
+        // console.log(projectNode);
+        const projectId = projectNode.getAttribute("data-projectid");
+        const listItemsToDelete = document.querySelectorAll(`div.toDos [data-projectid="${projectId}"]`);
+        listItemsToDelete.forEach((node) => {
+            const nodeParent = node.parentNode;
+            nodeParent.removeChild(node);
+        });
+    };
+
+    const deleteAllItemsv2 = () => {
+        const toDoSection = document.querySelector('.toDos');
+        while(toDoSection.firstChild) {
+            toDoSection.removeChild(toDoSection.firstChild);
+        }
+    };
+
+    // remove project as well as the to do list items
+    const removeProject = (projectNode) => {
+        // deleteAllItems(projectNode);
+        deleteAllItemsv2();
+        const parent = projectNode.parentNode;
+        parent.removeChild(projectNode);
+    };
+
+    const clearToDos = () => {
+        deleteAllItemsv2();
+    }
+
+    // adding toDos to a project
+    const addToProject = (projectId, item, itemId) => {
+        const toDoSection = document.querySelector('.toDos');
+        const toDo = document.createElement("div");
+        toDo.setAttribute("data-todoid", `${itemId}`);
+        toDo.setAttribute("data-projectid", `${projectId}`);
+        toDo.textContent = JSON.stringify(item);
+        toDoSection.appendChild(toDo);
+    };
+
+    // function to display project items in the DOM
+    // project: internal project object
+    // projectId: id of the project in the DOM
+    const viewProject = (project, projectId) => {
+        const toDos = project.getToDos();
+        toDos.forEach((toDo) => {
+            const item = {
+                title: toDo.getTitle(),
+                description: toDo.getDescription(),
+                dueDate: toDo.getDueDate(),
+                priority: toDo.getPriority(),
+            }   
+            addToProject(projectId, item, toDo.getId());
+        });
+    };
+
+    return { addProject, removeProject, clearToDos, viewProject, addToProject };
 
 })();
 
 
-// const displayController = (() => {
-//     const addProject = (project) => {
-    
-//     };
+// logic controller; calls the functions on the displayController as well as the internal toDoList module
+const logicController = (() => {
 
+    let currentProject = null;
+    const getNumProjects = () => toDoModule.getNumProjects();
+    const getNumToDos = (projectId) => toDoModule.getNumToDos(projectId);
 
+    const projects = ["One", "Two", "Three"];
 
-// })();
+    const addProjectButton = document.querySelector(".sidebar .addButton");
+    addProjectButton.addEventListener('click', (event) => {
+        // get the project name
+        // add to internal module as well as the display module
+        // set current project to the newly added project
+        projects.forEach((project) => {
+            const projectName = project;
+            const numProjects = getNumProjects();
+            toDoModule.addProject(projectName);
+            currentProject = displayController.addProject(projectName, numProjects);
+            currentProject.addEventListener('click', openProject);
+        });
+    });
 
-const today = toDoFactory('Work on to do', 'n/a', 'A week later', 'high');
-const newToDo = toDoFactory('Work on table', 'Finish assembling the table', 'Sometime next week', 'medium');
+    // function to run once a project has been clicked
+    // clear the current to do list and repopulate
+    const openProject = (event) => {
+        const projectNode = event.target;
+        const projectId = projectNode.getAttribute("data-projectid");
+        // console.log(projectNode);
+        displayController.clearToDos();
+        currentProject = projectNode;
+        // display internal project to do items
+        const project = toDoModule.viewProject(projectId);
+        // display the project to Do items in the DOM
+        displayController.viewProject(project, projectId);
+    };
 
-const homeProject = projectFactory('Home Project');
-homeProject.addToProject(today);
-homeProject.addToProject(newToDo);
-homeProject.printItems();
+    const addItemsToProjectDummy = (projectid, item) => {
+        const itemId = toDoModule.addToProject(projectid, item);
+        if(projectid == currentProject.getAttribute("data-projectid")){
+            displayController.addToProject(projectid, item, itemId);
+        }
+    };
 
-console.log("Now deleting an item from our project");
+    const addToDoButton = document.querySelector(".content .addButton");
+    addToDoButton.addEventListener('click', (event) => {
+        const projectId = Math.floor(Math.random() * 3);
+        const item = { title: 'Work on to do', description: 'n/a', dueDate: 'A week Later', priority: 'high' };
+        addItemsToProjectDummy(projectId, item);
+        const item2 = {title: 'Work on table', description: 'Finish assembling the table', dueDate: 'Sometime next week', priority: 'medium'};
+        addItemsToProjectDummy(projectId, item2);
+    });
 
-homeProject.removeFromProject(1);
-homeProject.printItems();
+})();
